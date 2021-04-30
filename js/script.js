020e5073baa6e45c4f5494c11a26024c5242a2cb
andriio <ostapenkoao@gmail.com>
2021-04-30 16:35:57 +0300

    lesson 15: cycles and basic conditions added

--- js/script.js
+++ js/script.js
@@ -21,10 +21,25 @@ const titles = [],
 //     }
 // while (i < 2);
 
-for (let i = 0; i < personalMovieDB.count; i++) {
+for (let i = 0; i < 2; i++) {
     titles[i] = prompt('Один из последних просмотренных фильмов?', '');
     ratings[i] = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[titles[i]] = ratings[i];    
+    if (titles[i] != '' && ratings[i] != '' && titles[i] != null && ratings[i] != null && titles[i].length < 50) {
+        personalMovieDB.movies[titles[i]] = ratings[i];  
+        console.log('done');
+    } else {
+        console.log("error");
+        i--;
+    }
 }
 
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
+    console.log("Вы классический зритель");
+} else {
+    console.log('Произошла ошибка');
+}
 console.log(personalMovieDB);
